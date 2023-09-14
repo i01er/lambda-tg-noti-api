e09db22edbb72a6a6107deacc2896ba36f6fbb90
chris.wan <chris.wan@soshk.com>
2023-09-14 12:28:09 +0800

    update code with asyncio

--- lambda_function.py
+++ lambda_function.py
@@ -1,20 +1,24 @@
 import json, os
 import telegram
+import asyncio
 # from dotenv import load_dotenv
 
 # load_dotenv()  # take environment variables from .env.
 
 async def lambda_handler(event, context):
     # TODO implement
+    loop = asyncio.get_event_loop()
+    return loop.run_until_complete(async_handler(event, context))
     
+async def async_handler(event, context):
     token = os.environ.get('bot_token')
     chatID = os.environ.get('chat_id')
 
     bot = telegram.Bot(token)
-    
+
     await bot.send_message(text='Testing!', chat_id=chatID)
     
     return {
         'statusCode': 200,
-        'body': json.dumps("finish")
+        'body': 'success'
     }
