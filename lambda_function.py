dc7905d0377f3993f40e36dfc65f957953b5b6ad
chris.wan <chris.wan@soshk.com>
2023-09-14 10:40:59 +0800

    add token to env var

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,14 @@
-import json
+import json, os
+import telegram
+# from dotenv import load_dotenv
+
+# load_dotenv()  # take environment variables from .env.
 
 def lambda_handler(event, context):
     # TODO implement
+    # bot = telegram.Bot(os.getenv("bot_token"))
+    token = os.environ.get['bot_token']
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from SOSHK!')
+        'body': json.dumps(token)
     }
