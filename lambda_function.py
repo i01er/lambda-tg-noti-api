070e618458a507285704fa588fe25fd7d5fbcc55
chris.wan <chris.wan@soshk.com>
2023-12-20 15:54:33 +0800

    update version

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,8 @@
 import json, os
 import telegram
 import asyncio
+import json
+from types import SimpleNamespace
 # from dotenv import load_dotenv
 
 # load_dotenv()  # take environment variables from .env.
@@ -8,15 +10,29 @@ import asyncio
 async def async_handler(event, context):
     token = os.environ.get('bot_token')
     chatID = os.environ.get('chat_id')
-
-    bot = telegram.Bot(token)
-
-    await bot.send_message(text=event["queryStringParameters"]['price'], chat_id=chatID)
     
-    return {
-        'statusCode': 200,
-        'body': 'success'
-    }
+    # print(event)
+    d_body = json.dumps(event)
+    body = json.loads(d_body)
+    msg = body["body"]
+    temp_msg = json.loads(msg)
+    
+    if(event and temp_msg["pwd"] == "testing123"):
+        # print(event)
+        bot = telegram.Bot(token)
+        await bot.send_message(text=temp_msg["text"], chat_id=chatID)
+    
+        return {
+            'statusCode': 200,
+            'headers': {'Content-Type': 'application/json'},
+            'body': 'success'
+        }
+    else:
+        return {
+            'statusCode': 400,
+            'headers': {'Content-Type': 'application/json'},
+            'body': 'fail'
+        }
 
 def lambda_handler(event, context):
     # TODO implement
